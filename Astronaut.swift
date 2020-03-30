c8b9d1eeba4c4cd1030111dd87ab217c162ffef1
Stefan Steinbauer <stst@usinglight.com>
2020-03-30 16:27:50 +0200

    Initial commit

---
+++ Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Stefan Steinbauer on 30.03.20.
+//  Copyright © 2020 Stefan Steinbauer. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
